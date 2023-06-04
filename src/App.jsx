b5849e1985f81b7dda77bbcef8f06e55bf4bb496
John Doe <johndoe@example.com>
2023-06-04 06:57:56 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function App() {
-  const[color,setColor]=useState()
-  const handleColor=(e)=>{
-    const{value}=e.target
-    setColor(value)
-  }
+  const [color, setColor] = useState();
+  const handleColor = (e) => {
+    const { value } = e.target;
+    setColor(value);
+  };
+  console.log(van);
   return (
     <div className="h-screen bg-slate-400">
-    <h1 style={{color:`${color}`}} className="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>
-    <form action="">
-      <input type="color" name="" id=""  onChange={handleColor}/>
-    </form>
+      <h1
+        style={{ color: `${color}` }}
+        className="text-3xl font-bold underline text-center"
+      >
+        Hello world!
+      </h1>
+      <form action="">
+        <input type="color" name="" id="" onChange={handleColor} />
+        <input type="datetime-local" name="" id=""  />
+      </form>
     </div>
-  )
+  );
 }
