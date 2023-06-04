2303fd7ad37ce759c1324294b5622e171a727525
John Doe <johndoe@example.com>
2023-06-04 06:47:32 +0530

    b

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+
 export default function App() {
+  const[color,setColor]=useState()
+  const handleColor=(e)=>{
+    const{value}=e.target
+    setColor(value)
+  }
   return (
     <div className="h-screen bg-slate-400">
-    <h1 className="text-3xl font-bold underline text-center">
+    <h1 style={{color:`${color}`}} className="text-3xl font-bold underline text-center">
       Hello world!
     </h1>
+    <form action="">
+      <input type="color" name="" id=""  onChange={handleColor}/>
+    </form>
     </div>
   )
 }
