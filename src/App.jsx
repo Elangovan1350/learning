2ff22981e622d2a02411a5659a6853098bc5c5c9
John Doe <johndoe@example.com>
2023-06-04 06:35:48 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
+    <div className="h-screen bg-slate-400">
     <h1 className="text-3xl font-bold underline text-center">
       Hello world!
     </h1>
+    </div>
   )
 }
